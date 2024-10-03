d3d1a75a6f3d406bdecaeec15a95345f670b1e28
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 12:52:43 +0100

    masdf

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''A module containing pascal triangle function
+'''
+
+def pascal_triangle(n):
+    '''Create a pascal triangle and n represent the number of rows'''
+    triangle = []
+    if n <=0:
+        return triangle
+    
+    if n ==1:
+        return [1]
+    
+    if n ==2:
+        return [1,1]
+    
+    triangle = [[1], [1, 1]]
+    
+    for i in range(2, n):
+        triangle.append([1])
+        for j in range(i - 1):
+            triangle[i].append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        triangle[i].append(1)
+
+    return(triangle)
+
+    
