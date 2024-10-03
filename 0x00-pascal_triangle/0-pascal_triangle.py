088db09576e5b1ae64aa2e041f996f05dc4e5e57
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 13:04:53 +0100

    sdf

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-'''A module containing pascal triangle function
+'''A module containing the Pascal's triangle function
 '''
 
 def pascal_triangle(n):
-    '''Create a pascal triangle and n represent the number of rows'''
+    '''Create a Pascal's triangle with `n` representing the number of rows.'''
     triangle = []
-    if n <=0:
+    
+    if n <= 0:
         return triangle
     
-    if n ==1:
-        return [[1]]
+    triangle = [[1]]
     
-    if n ==2:
-        return [[1], [1, 1]]
-    
-    triangle = [[1], [1, 1]]
-    
-    for i in range(2, n):
-        triangle.append([1])
-        for j in range(i - 1):
-            triangle[i].append(triangle[i - 1][j] + triangle[i - 1][j + 1])
-        triangle[i].append(1)
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
 
-    return(triangle)
+    return triangle
