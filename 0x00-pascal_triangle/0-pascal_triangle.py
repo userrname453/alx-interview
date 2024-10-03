2bb7f02156ae618b210c5c40be2034e9d652573a
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 13:20:53 +0100

    hjk

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -20,5 +20,4 @@ def pascal_triangle(n):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    print(triangle)
     return triangle
