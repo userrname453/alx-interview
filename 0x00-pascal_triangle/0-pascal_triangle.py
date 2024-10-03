c0730684109954855139910054647862d8500439
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 13:06:43 +0100

    sfd

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,13 +2,16 @@
 '''A module containing the Pascal's triangle function
 '''
 
+
 def pascal_triangle(n):
-    '''Create a Pascal's triangle with `n` representing the number of rows.'''
+    '''Create a Pascal's triangle with `n`
+    representing the number of rows.
+    '''
     triangle = []
     
-    if n <= 0:
+    if type(n) is not int or n <= 0:
         return triangle
-    
+
     triangle = [[1]]
     
     for i in range(1, n):
@@ -17,5 +20,4 @@ def pascal_triangle(n):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-
     return triangle
