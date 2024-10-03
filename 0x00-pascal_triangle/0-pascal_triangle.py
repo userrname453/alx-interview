1634335bff4f30b504e753f17c5de91dc929325b
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 13:29:35 +0100

    fg

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,14 +8,11 @@ def pascal_triangle(n):
     representing the number of rows.
     '''
     triangle = []
-    
     if type(n) is not int or n <= 0:
         return triangle
-
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
-    
     return triangle
