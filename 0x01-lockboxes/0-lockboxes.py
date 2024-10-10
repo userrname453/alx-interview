31ebab605f7d07cc577a788c602f7005b5b55cb3
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-10 20:34:15 +0100

    sdf

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
+'''A module for determining if all boxes can be unlocked.'''
 
 def canUnlockAll(boxes):
-    n = len(boxes)
-    discovered_boxes = set([0])
-    hidden_box = set(boxes[0]).difference(set([0]))
-    while len(hidden_box) > 0:
-        boxIdx = hidden_box.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
+    '''Determines if all the boxes can be unlocked. Each box contains keys 
+    (indices) that unlock other boxes. The first box is initially unlocked.
+    
+    Args:
+        boxes (list of lists): A list where each element is a list of keys (box indices).
+        
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    '''
+    total_boxes = len(boxes)  # Total number of boxes
+    unlocked_boxes = set([0])  # Set of boxes that have been unlocked, starting with the first
+    keys_to_check = set(boxes[0]).difference(set([0]))  # Keys found in the first box, excluding the first box itself
+
+    while len(keys_to_check) > 0:  # Loop while there are still keys to check
+        current_key = keys_to_check.pop()  # Get a key from the keys_to_check set
+        if current_key < 0 or current_key >= total_boxes:  # Ignore invalid box indices
             continue
-        if boxIdx not in discovered_boxes:
-            hidden_box = hidden_box.union(boxes[boxIdx])
-            discovered_boxes.add(boxIdx)
-    return n == len(discovered_boxes)
+        if current_key not in unlocked_boxes:  # If this box has not been unlocked yet
+            keys_to_check = keys_to_check.union(boxes[current_key])  # Add new keys from this box
+            unlocked_boxes.add(current_key)  # Mark this box as unlocked
+    
+    return total_boxes == len(unlocked_boxes)  # All boxes are unlocked if the number of unlocked boxes matches the total
