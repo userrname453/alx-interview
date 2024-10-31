1f267dd28137c00f7ed05de140a10b04bf79bc95
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-31 12:50:20 +0100

    fdg

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,57 +1,55 @@
 #!/usr/bin/python3
-"""Module to validate UTF-8 encoded data."""
+"""Module to validate UTF-8 encoded data.
+"""
 
-def isValidUTF8(data):
+
+def validUTF8(data):
     """Determines if a list of integers represents valid UTF-8 encoded characters.
-    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    remaining_bytes = 0
-    data_len = len(data)
-    for idx in range(data_len):
-        if remaining_bytes > 0:
-            remaining_bytes -= 1
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
             continue
-        if type(data[idx]) != int or data[idx] < 0 or data[idx] > 0x10ffff:
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
             return False
-        elif data[idx] <= 0x7f:
-            remaining_bytes = 0
-        elif data[idx] & 0b11111000 == 0b11110000:
-            # 4-byte sequence in UTF-8
-            byte_count = 4
-            if data_len - idx >= byte_count:
-                following_bytes = list(map(
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[idx + 1: idx + byte_count],
+                    data[i + 1: i + span],
                 ))
-                if not all(following_bytes):
+                if not all(next_body):
                     return False
-                remaining_bytes = byte_count - 1
+                skip = span - 1
             else:
                 return False
-        elif data[idx] & 0b11110000 == 0b11100000:
-            # 3-byte sequence in UTF-8
-            byte_count = 3
-            if data_len - idx >= byte_count:
-                following_bytes = list(map(
+        elif data[i] & 0b11110000 == 0b11100000:
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[idx + 1: idx + byte_count],
+                    data[i + 1: i + span],
                 ))
-                if not all(following_bytes):
+                if not all(next_body):
                     return False
-                remaining_bytes = byte_count - 1
+                skip = span - 1
             else:
                 return False
-        elif data[idx] & 0b11100000 == 0b11000000:
-            # 2-byte sequence in UTF-8
-            byte_count = 2
-            if data_len - idx >= byte_count:
-                following_bytes = list(map(
+        elif data[i] & 0b11100000 == 0b11000000:
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[idx + 1: idx + byte_count],
+                    data[i + 1: i + span],
                 ))
-                if not all(following_bytes):
+                if not all(next_body):
                     return False
-                remaining_bytes = byte_count - 1
+                skip = span - 1
             else:
                 return False
         else:
